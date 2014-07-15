fe261a377fe93b1eea4be248cc1d6a130a79ae4c
Christopher Bunn <b11.chris@gmail.com>
2014-07-14 21:41:25 -0500

    Added skeleton Travis-CI support

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from setuptools import find_packages
 
-setup(name='django-citadel',
-      version='0.1.1',
-      description='Django Citadel - providing an encrypted ModelField',
-      author='Christopher Bunn',
-      author_email='',
-      url='',
-      packages=['citadel'],
-     )
+setup(
+    name='django-citadel',
+    version='0.1.2',
+    description='Django Citadel - providing an encrypted ModelField',
+    author='Christopher Bunn',
+    author_email='',
+    url='',
+    packages=find_packages(),
+    install_requires = [
+        'pycrypto',
+    ],
+)
